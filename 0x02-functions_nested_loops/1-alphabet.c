59c19e39d66c93c3877573c8987b81dcf12fe72a
santos0007 <shingsantonio@gmail.com>
2023-02-22 11:44:43 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print - make the alphabet in lower case
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - checking the code
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
